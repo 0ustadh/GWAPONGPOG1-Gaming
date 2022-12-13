ce1cf70b40d73eca3dabd1f012368a783a14481c
0ustadh <delatorreivanvincent19@gmail.com>
2022-12-13 21:13:02 +0800

    Uploading this project to Github

---
+++ landingpage.js
@@ -0,0 +1,13 @@
+// Submenu
+$(function() {
+    $(".btn2").click(function() {
+        $(".submenu").slideToggle(500);
+    });
+}); 
+//Submenu end
+
+// Tooltip Initialization
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+  });
+// Tooltip Initialization end
